53692c6115c8066bd872f4860d3c3aa0f728f7aa
Brian Shimkus <brianshimkus1@gmail.com>
2021-01-29 23:28:02 -0600

    Created Navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Create Next App</title>
+				<title>Authenticated Todo App</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
+
+			<Navbar />
 
 			<main>
 				<h1>Todo</h1>
